ca9901f70b20f0e1b0099a242de4af1d69e7c894
tutdeputraw <anoboy.code@gmail.com>
2021-10-24 16:08:06 +0700

    syncing database / migrate

--- server.js
+++ server.js
@@ -2,34 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const sequelize = require('./utils/database');
+const errorController = require('./controllers/errorController');
+
+const post = require('./models/post');
+
 const feedRoutes = require('./routes/postRoutes');
 
 const app = express();
 
 app.use(bodyParser.json());
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-
 app.use(morgan('dev'));
 
 app.use('/post', feedRoutes);
+app.use(errorController.get404);
 
-app.use((req, res, next) => {
-  const error = new Error('Not Found');
-  error.status(404);
-  next(error);
-});
-app.use((error, req, res, next) => {
-  res.status(404).json({
-    error: {
-      message: error.message
-    }
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
   })
-});
-
-app.listen(3000);
+  .catch(err => {
+    console.log(err);
+  });
